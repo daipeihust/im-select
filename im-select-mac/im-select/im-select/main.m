42d2628c78de2d05837ef96840531e6f6faab95f
DaiPei <daipei@hust.edu.cn>
2018-05-29 14:44:33 +0800

    use __bridge instead of transfer or retain

--- im-select-mac/im-select/im-select/main.m
+++ im-select-mac/im-select/im-select/main.m
@@ -10,25 +10,25 @@
 #import <Carbon/Carbon.h>
 
 int main(int argc, const char * argv[]) {
-    int returnCode = 0;
+    int ret = 0;
     @autoreleasepool {
         if (argc > 1) {
             NSString *inputSource = [NSString stringWithUTF8String:argv[1]];
             NSDictionary *filter = [NSDictionary dictionaryWithObject:inputSource forKey:(NSString *)kTISPropertyInputSourceID];
-            CFArrayRef keyboards = TISCreateInputSourceList((__bridge_retained CFDictionaryRef)filter, false);
+            CFArrayRef keyboards = TISCreateInputSourceList((__bridge CFDictionaryRef)filter, false);
             if (keyboards) {
                 TISInputSourceRef selected = (TISInputSourceRef)CFArrayGetValueAtIndex(keyboards, 0);
-                returnCode = TISSelectInputSource(selected);
+                ret = TISSelectInputSource(selected);
                 CFRelease(keyboards);
             } else {
-                returnCode = 1;
+                ret = 1;
             }
         } else {
             TISInputSourceRef currentInputSource = TISCopyCurrentKeyboardInputSource();
-            NSString *sourceId = (__bridge_transfer NSString *)(TISGetInputSourceProperty(currentInputSource, kTISPropertyInputSourceID));
+            NSString *sourceId = (__bridge NSString *)(TISGetInputSourceProperty(currentInputSource, kTISPropertyInputSourceID));
             printf("%s\n", [sourceId UTF8String]);
             CFRelease(currentInputSource);
         }
     }
-    return returnCode;
+    return ret;
 }
