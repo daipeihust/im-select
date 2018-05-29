8c7aa961035675a077071d714961ddfffcaa5bf8
DaiPei <daipei@hust.edu.cn>
2018-05-29 15:33:25 +0800

    add install_mac.sh

---
+++ install_mac.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# if im-select already exist, delete it and download it again
+if [ -e /usr/local/bin/im-select ]
+then
+    rm -f /usr/local/bin/im-select
+fi
+
+echo "* Downloading im-select..."
+
+curl -Ls -o /usr/local/bin/im-select https://github.com/daipeihust/im-select/raw/master/im-select-mac/out/im-select
+
+chmod 777 /usr/local/bin/im-select
+
+echo "* im-select is installed!"
